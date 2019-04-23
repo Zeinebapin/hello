ed261c3b29343e2a9169274f70d8cf4f759f2323
zeineb bap <zbapin@gmail.com>
2019-04-23 12:04:50 +0200

    modification liste

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ $resultat = getAnnonces();
 <ul>
     <li><?php echo $resultat["annonce1"][0] ?></li>
     <li><?php echo $resultat["annonce1"][1] ?></li>
+</ul>
+<ul>
     <li><?php echo $resultat["annonce2"][0] ?></li>
     <li><?php echo $resultat["annonce2"][1] ?></li>
 </ul>
