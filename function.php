4affea26c5aed6f68a8a3fbc84358314f8fb6094
zeineb bap <zbapin@gmail.com>
2019-04-24 09:46:19 +0200

    mise a jour

--- function.php
+++ function.php
@@ -1,16 +1,21 @@
-
-<?php
-function getAnnonces()
-{
+<?php 
+function getAllAnnonces() {
     $data = [
-        "annonce1" => ["voiture", 1000] ,
-        "annonce2" => ["meuble", 55] 
+        "annonce1" => ["voiture", 1000],
+        "annonce2" => ["meuble", 500]
     ];
     return $data;
 }
-
-function getFirstAnnonce(){
-	$first = current(getAnnonces());
-	return $first;
+function getFirstAnnonce() {
+    $first = current(getAllAnnonces());
+    return $first ;
 }
+
+function insertAnnonce( $libelle, $tab) {
+    $data = getAllAnnonces();
+    $data[$libelle] = $tab;
+    return $data;
+}
+
+
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,43 @@
-<?php
-require "function.php" ;
+<?php 
+require_once "function.php";
 
-$resultat = getAnnonces();
+$resultat = getAllAnnonces();
 
 ?>
-
-<ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="js/jquery-3.4.0.min.js"></script>
+<script src="js/script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <ul>
     <li><?php echo $resultat["annonce1"][0] ?></li>
     <li><?php echo $resultat["annonce1"][1] ?></li>
 </ul>
+<hr>
 <ul>
     <li><?php echo $resultat["annonce2"][0] ?></li>
+    <li><?php echo $resultat["annonce2"][1] ?></li>
 </ul>
+</body>
+</html>
+
+<script>
+    $('ul').html("yoyoyo");
+</script>
+
+<?php 
+
+$first = getFirstAnnonce();
+
+print_r($first);
+
+$newProduit = insertAnnonce("annonce3", array("fruit",2));
+echo '<pre>';
+print_r($newProduit);
+?>
